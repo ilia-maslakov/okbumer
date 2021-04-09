b6cbf261e7cc420bb147d7f5e5506a99412aeb33
Ilia Maslakov <il.smind@gmail.com>
2021-04-09 17:47:05 +0300

    Create Program.cs

---
+++ okbumer/Program.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace okbumer
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            var frm = new Form1();
+            Application.Run(frm);
+
+        }
+    }
+}
